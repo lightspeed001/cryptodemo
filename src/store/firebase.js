ed31e8a6cf051e7ac307bdce099ab1792d4fc9a9
Your Name <you@example.com>
2018-02-28 03:41:11 +0200

    commit three

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -8,12 +8,7 @@ import fb from "firebase"
 //const msg = firebase.messaging();
 
 var config = {
-    apiKey: "AIzaSyDQfNrVzZZDfdD72tHokTPXZLwiec0BsVc",
-    authDomain: "fir-7fd3c.firebaseapp.com",
-    databaseURL: "https://fir-7fd3c.firebaseio.com",
-    projectId: "fir-7fd3c",
-    storageBucket: "fir-7fd3c.appspot.com",
-    messagingSenderId: "943050628977"
+
   };
 var firebase = fb.initializeApp(config);
 console.log('fb initialized!!')
